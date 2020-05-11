692ff62eb2e23bd06de7954757276d7c607a2fdc
muhammadarif <muhammad.arif5995@gmail.com>
2020-05-11 13:56:05 +0700

    Initial commit

---
+++ lib/ui/widget/header.dart
@@ -0,0 +1,88 @@
+import 'package:covid19/constant.dart';
+import 'package:covid19/ui/info_page.dart';
+import 'package:covid19/ui/widget/clipper.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Header extends StatefulWidget {
+  final String image;
+  final String textTop;
+  final String textBottom;
+  final double offset;
+
+  const Header(
+      {Key key, this.image, this.textTop, this.textBottom, this.offset})
+      : super(key: key);
+
+  @override
+  _HeaderState createState() => _HeaderState();
+}
+
+class _HeaderState extends State<Header> {
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: MyClipper(),
+      child: Container(
+        padding: EdgeInsets.only(left: 40, top: 50, right: 20),
+        height: 350,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Color(0xFF3383CD),
+                Color(0xFF11249F),
+              ]),
+          image: DecorationImage(
+            image: AssetImage("assets/images/virus.png"),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return InfoScreen();
+                }));
+              },
+              child: SvgPicture.asset("assets/icons/menu.svg"),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    top: (widget.offset < 0) ? 0 : widget.offset,
+                    child: SvgPicture.asset(
+                      widget.image,
+                      width: 230,
+                      fit: BoxFit.fitWidth,
+                      alignment: Alignment.topCenter,
+                    ),
+                  ),
+                  Positioned(
+                    top: 29 - widget.offset / 2,
+                    left: 140,
+                    child: Text(
+                      "${widget.textTop} \n${widget.textBottom}",
+                      style: kHeadingTextStyle.copyWith(
+                        color: Colors.white,
+                        fontSize: 20
+                      ),
+                    ),
+                  ),
+                  Container(), // I dont know why it can't work without container
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
